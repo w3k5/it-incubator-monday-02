040a640820c07a4cf40168eb5f99d226546b9722
Vladislav Marchenko <w3k5@yandex.ru>
2022-08-18 04:13:10 +0300

    validate is blogger exists when create post in validator

--- src/validators/post-validators/blogger-id-validator.ts
+++ src/validators/post-validators/blogger-id-validator.ts
@@ -1,5 +1,15 @@
 import { ValidationChain } from 'express-validator';
+import { bloggersRepository } from '../../index';
 
 export const bloggerIdValidator = (chain: ValidationChain): ValidationChain => {
-	return chain.isNumeric().withMessage('bloggerId must be a number!');
+	return chain
+		.isNumeric()
+		.withMessage('bloggerId must be a number!')
+		.custom(async (value) => {
+			const bloggerCandidate = await bloggersRepository.getById(value);
+			if (!bloggerCandidate) {
+				throw new Error('Blogger with that ID is not exists!');
+			}
+			return true;
+		});
 };
