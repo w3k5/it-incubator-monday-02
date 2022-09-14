e2dfa50c205f044546fa7696db4ea5dec9641b0c
Vladislav Marchenko <w3k5@yandex.ru>
2022-09-14 12:41:22 +0300

    Refactoring

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,1 @@
 export * from './error.interface';
-export * from './repository.interface';
-export * from './no-sql-repository.interface';
---
+++ src/modules/post/__tests__/post.serivce.spec.ts
@@ -0,0 +1,5 @@
+describe('Post Service tests', () => {
+	it('Should test Create Post', () => {
+		expect(1).toBe(2);
+	});
+});
