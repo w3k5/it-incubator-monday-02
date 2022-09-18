810a2ee45764e1d68dda6d1aee1bcde93dbaeab2
Vladislav Marchenko <w3k5@yandex.ru>
2022-09-18 13:13:33 +0300

    Inited nestjs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import { runDb } from './db';
-import { app } from './app';
-import { Container } from 'inversify';
-import { controller, httpGet, interfaces, InversifyExpressServer, request, response } from 'inversify-express-utils';
-import express from 'express';
-import { iocContainer } from './_inversify/inversify.config';
+import { app as expressApp } from './app';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './modules/app/app.module';
 
 const port = process.env.PORT || 3000;
 
 const startApp = async () => {
 	await runDb();
-
-	app.listen(port, () => {
-		console.log(` IT-Incubator homework monday-2 has been started at port: ${port} `);
-	});
+	const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+	await app.listen(port);
+	console.log(`IT-Incubator homework monday-2 has been started at port: ${port}`);
 };
 
 startApp().catch((error) => {
